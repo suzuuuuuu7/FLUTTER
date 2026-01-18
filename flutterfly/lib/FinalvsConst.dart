6435beba3cd25de30d32bb657680fed8af10a228
suzuuuuuu7 <sujalkc324@gmail.com>
2026-01-18 20:30:34 +0545

    FinalVsConst in dart

---
+++ flutterfly/lib/FinalvsConst.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "sujal kc";
+  name = "Rajan bhdandari"; // Thiis is acceptance
+  print("$name");
+  final address;
+  // final var id;  var also cannot be used because we used var keyboard for dynamic initialization
+  // address = "Baglung"; This cannot be done because of final keyword
+  //const sem; This is incorrect because const is same as final but we often use const keyboard for inline data initialization
+  const String sem = "Fifth";
+  print(sem);
+}
