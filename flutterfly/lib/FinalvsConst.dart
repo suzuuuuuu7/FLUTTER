92aa75fbb00d22b38a59f5f704a424cb0dc0fbb8
suzuuuuuu7 <sujalkc324@gmail.com>
2026-02-21 16:05:06 +0545

    updated

--- flutterfly/lib/FinalvsConst.dart
+++ flutterfly/lib/FinalvsConst.dart
@@ -1,11 +1,22 @@
 void main() {
-  String name = "sujal kc";
-  name = "Rajan bhdandari"; // Thiis is acceptance
-  print(name);
-  final address;
-  // final var id;  var also cannot be used because we used var keyboard for dynamic initialization
-  // address = "Baglung"; This cannot be done because of final keyword
-  //const sem; This is incorrect because const is same as final but we often use const keyboard for inline data initialization
-  const String sem = "Fifth";
-  print(sem);
+  print("Hello flutter!");
+  final String name = "sujal kc"; // we can assign value of variable at the name of declaration or later
+
+    // name = "suman dangii" // we cannot reassign value if we use final keyword
+  final int age ;
+  age = 30;  
+  // age = 33;// we cannot reassign value if we use final keyword
+
+// we cannot reassign value if we use final keyword
+
+ 
+  // const address; // must be assgin value at the time of declaration
+  const String Address = "Nepal";
+  // Address = "India"; // we cannot reassign value if we use const keyword
+
+final list = [2,3,5,4,5];
+list.add(10); // we can add values in list
+const list1 = ["sujal","manoj"];
+// list1.add("Aakash"); // we will get run time error so we cannot add values in case of const keyword
+
 }
