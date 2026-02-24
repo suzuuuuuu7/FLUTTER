4517fa60ca26ab3af10ac5bdcee6caf55593dedc
suzuuuuuu7 <sujalkc324@gmail.com>
2026-02-24 14:44:30 +0545

    Updated

--- flutterfly/lib/switch.dart
+++ flutterfly/lib/switch.dart
@@ -53,7 +53,7 @@ class MyPage extends StatelessWidget {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text("flutter"),
       ),
