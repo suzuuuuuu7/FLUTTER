562bf22020f8da385021c7c0a207e35e57312393
suzuuuuuu7 <sujalkc324@gmail.com>
2026-02-28 16:06:10 +0545

    updated

--- flutterfly/lib/statechange.dart
+++ flutterfly/lib/statechange.dart
@@ -7,77 +7,124 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage(),
-      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  State<StatefulWidget> createState() {
-    return Homepagestate();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class Homepagestate extends State<HomePage> {
-  var count = 0;
-  var count1 = 0;
+class _MyHomePageState extends State<MyHomePage> {
   @override
+  var no1Controller = TextEditingController();
+  var no2Controller = TextEditingController();
+  var result = "";
   Widget build(BuildContext context) {
-    print("Build method called");
+    print("rebuld is called");
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "State Change Demo",
-          style: TextStyle(color: Colors.deepOrange, fontSize: 25),
-        ),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("flutter"),
       ),
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 400,
-          color: Colors.blueGrey,
+      body: Container(
+        color: Colors.blue.shade100,
+        child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Counter: $count",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.indigo,
-                  )),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      count++;
-                      print(count);
-                    });
-                  },
-                  child: Text("Increment count")),
-              SizedBox(
-                height: 20,
+              TextField(
+                controller: no1Controller,
+                keyboardType: TextInputType.number,
               ),
-              Text("Counter:$count1",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.yellow,
-                      fontWeight: FontWeight.w800)),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      count1--;
-                      print(count1);
-                    });
-                  },
-                  child: Text("Decrement count"))
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: no2Controller,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                        var no1 = int.parse(no1Controller.text.toString());
+                        var no2 = int.parse(no2Controller.text.toString());
+                        var sum = no1 + no2;
+                        result = "The sum of $no1 and $no2 is $sum";
+                      },
+                      child: Text("add"),
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text("mul")),
+                    ElevatedButton(onPressed: () {}, child: Text("sub")),
+                    ElevatedButton(
+                        onPressed: () {
+                          var no1 = int.parse(no1Controller.text.toString());
+                          var no2 = int.parse(no2Controller.text.toString());
+                          var div = no1 / no2;
+                          result = "The resulti is : ${div.toStringAsFixed(3)}";
+                          setState(() {});
+                        },
+                        child: Text("div")),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(21),
+                child: Text(
+                  result,
+                  style: TextStyle(fontWeight: FontWeight(1000)),
+                ),
+              ),
             ],
           ),
         ),
